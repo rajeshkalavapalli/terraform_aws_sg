658ee9340b195c0a5416fe3ed847a5832708e6f1
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-12 22:43:04 +0530

    aws-sg

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "sg_name" {
+  
+}
+
+variable "sg_description" {
+  default = ""
+  type = string
+
+}
+
+variable "project_name" {
+   
+  
+}
+
+variable "environment" {
+    
+}
+
+variable "common_tags" {
+  type = map 
+  default = {}
+  
+}
+
+variable "sg_tags" {
+  type = map 
+  default = {}
+}
+
+variable "vpc_id" {
+
+  
+}
+
+variable "sg_ingress_rules" {
+  type = list
+  default = []
+}
